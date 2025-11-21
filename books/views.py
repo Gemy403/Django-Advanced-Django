c0bda14420896a9efcc8f7ebbaa741854765f60d
Gemy403 <ahmedclenton@gmail.com>
2025-11-21 17:09:08 +0300

    add book list view + template

--- books/views.py
+++ books/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Book
 # Create your views here.
+
+
+def book_list(request):
+    data = Book.objects.all()
+    return render(request,'books/all_books.html',{'books':data})
--- project/urls.py
+++ project/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from books.views import book_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('book_list/', book_list),
 ]
 
 
