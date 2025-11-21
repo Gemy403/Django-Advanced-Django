6c498c1a09328f51c023046c441fc6d14d5e721e
Gemy403 <ahmedclenton@gmail.com>
2025-11-21 18:54:35 +0300

    add average rate template filter

--- books/templatetags/custome_filter.py
+++ books/templatetags/custome_filter.py
@@ -5,3 +5,14 @@ register = template.Library()
 @register.filter
 def format_price(value):
     return f'$ {value}'
+
+
+@register.filter
+def average_rate(reviews):
+    total = sum((review.rate for review in reviews))
+    count = len(reviews)
+    if count > 0 :
+        avg = round(total / count,2)
+        return avg
+    else:
+        return 0
